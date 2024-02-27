ad16c7ad14f29f12bb4851273c1e58cc2639eae6
someguy163 <wnguddl163@naver.com>
2024-02-27 10:42:28 +0900

    2024-02-27

--- ch03_pjt_02/src/main/java/ch03_pjt_02/MainClass.java
+++ ch03_pjt_02/src/main/java/ch03_pjt_02/MainClass.java
@@ -1,12 +1,20 @@
 package ch03_pjt_02;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		TransportaionWalk transportaionWalk = new TransportaionWalk();
+//		TransportaionWalk transportaionWalk = new TransportaionWalk();
+//		transportaionWalk.move();
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		TransportationWalk transportaionWalk = ctx.getBean("tWalk",TransportationWalk.class);
 		transportaionWalk.move();
+		
+		ctx.close();
 	}
 
 }
--- ch03_pjt_02/src/main/java/ch03_pjt_02/TransportationWalk.java
+++ ch03_pjt_02/src/main/java/ch03_pjt_02/TransportationWalk.java
@@ -1,6 +1,6 @@
 package ch03_pjt_02;
 
-public class TransportaionWalk {
+public class TransportationWalk {
 	
 	public void move() {
 		System.out.println("도보로 이동합니다");
