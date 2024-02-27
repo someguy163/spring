8f1490c5bca69b7e7c869b23a171674342a319a3
someguy163 <wnguddl163@naver.com>
2024-02-27 09:35:31 +0900

    2024-02-27

---
+++ ch03_pjt_01/src/main/java/ch03_pjt_01/TransportaionWalk.java
@@ -0,0 +1,9 @@
+package ch03_pjt_01;
+
+public class TransportaionWalk {
+	
+	public void move() {
+		System.out.println("도보로 이동합니다");
+	}
+
+}
---
+++ ch03_pjt_02/src/main/java/ch03_pjt_02/MainClass.java
@@ -0,0 +1,12 @@
+package ch03_pjt_02;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TransportaionWalk transportaionWalk = new TransportaionWalk();
+		transportaionWalk.move();
+	}
+
+}
